15881184bdf8bdedab1031be7bca5156d37113fc
Vee-nimation <vincentakporhuarho@gmail.com>
2022-09-12 13:07:31 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,21 +2,8 @@
 #include <unistd.h>
 /**
 * main - Entry point
-* Description: prints numbers
+* Description: prints two digits combination
 * Return: Always 0 (success)
 */
 int main(void)
 {
-int i;
-for (i = '0'; i <= '9' ; i++)
-{
-putchar(i);
-if (i != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-}
