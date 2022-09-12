6fb82125121fad388bdccfd726db8de8ee0d504b
Vee-nimation <vincentakporhuarho@gmail.com>
2022-09-12 13:49:25 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,8 +2,21 @@
 #include <unistd.h>
 /**
 * main - Entry point
-* Description: prints two digits combination
+* Description: prints numbers
 * Return: Always 0 (success)
 */
 int main(void)
 {
+int i;
+for (i = '0'; i <= '9' ; i++)
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
