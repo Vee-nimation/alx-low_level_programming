0cb071864072c4ff873bcf3219f81762081a81da
Vee-nimation <vincentakporhuarho@gmail.com>
2022-09-30 18:56:09 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day,
+
+my goal was to be the best
+
+hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
