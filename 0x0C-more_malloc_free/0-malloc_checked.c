227c816d2c53d52c6a16fc7ccd022e5f97fc08c0
Vee-nimation <vincentakporhuarho@gmail.com>
2022-10-06 14:15:18 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,6 @@
 /**
  * malloc_checked - A function that allocates memory using malloc.
  * @b: integer variable dat holds the memory size
- * Author: Sammykingx
  * Return: 0 means program was successful
  */
 void *malloc_checked(unsigned int b)
